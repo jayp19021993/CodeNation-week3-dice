f85b9976e9eee11aadeee2175a4b2dd97b96c496
Jay Patel <jay_patel_123@hotmail.co.uk>
2022-01-05 16:29:08 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let rollDice , score
+
+init();
+
+
+function rollDice(){
+ const rollDice= Math.floor(Math.random()*6 +1); 
+ 
+score += rollDice
+console.log();
+
+ if (rollDice === 1 ) 
+ console.log("you losse"); 
+
+ 
+ else{ 
+(score ===20)   
+ console.log ("you win");
+ }
+
+}
